4a22069fea95f70671979e15176a83f0eb965535
Kuba Clark <jakub.clark@protonmail.com>
2019-07-12 03:13:54 +0200

    Simplify to_human_json

--- human_json/transformer.py
+++ human_json/transformer.py
@@ -1,69 +1,62 @@
-from typing import Dict, List, Tuple, Any, Union
+from typing import Any, Dict, List, Tuple, Union
 
 BASE_TYPES = (str, int, float, bool)
 
+ALLOWED_TYPES = BASE_TYPES + (dict, list, tuple)
 
-def to_human_json(data: Dict, indent: Union[str, int] = '\t', prefix: str = '', new_line: str = '\n'):
+
+def to_human_json(json_data: Any, indent: Union[str, int] = '\t', prefix: str = '', new_line: str = '\n'):
     """
     Prettify a JSON Object
-    :param data: The JSON Object to prettify. Must be a dictionary
+    :param json_data: The JSON Object to prettify.
     :param indent: Optional indent string ('\t') or number of spaces to indent, at each new level
     :param prefix: Optional prefix, prepended to each line ('*')
     :param new_line: Optional new_line character/string
     :return: A pretty string of the incoming JSON Object
     """
-    if not isinstance(data, Dict):
-        raise TypeError(f'`data` must be a {dict}. It is {type(data)} instead')
+
+    def _serialize(data: Any, depth: int):
+        lines = []
+
+        full_prefix = f'{indent * depth}{prefix}'
+
+        if isinstance(data, BASE_TYPES):
+            lines.append(f'{full_prefix}{data}')
+
+        elif data is None:
+            lines.append(f'{full_prefix}{None}')
+
+        elif isinstance(data, (List, Tuple)):
+            if data:
+                if len(data) == 1:
+                    lines.append(f'{full_prefix}{data[0]}')
+                else:
+                    for entry in data:
+                        lines.extend(_serialize(entry, depth))
+            else:
+                lines.append(f'{full_prefix}-')
+
+        elif isinstance(data, Dict):
+            if data:
+                for k, v in data.items():
+                    if isinstance(v, BASE_TYPES):
+                        lines.append(f'{full_prefix}{k}: {v}')
+
+                    elif v is None:
+                        lines.append(f'{full_prefix}{k}: {None}')
+
+                    else:
+                        lines.append(f'{full_prefix}{k}:')
+                        lines.extend(_serialize(v, depth + 1))
+            else:
+                lines.append(f'{full_prefix}-')
+
+        return lines
+
+    if not isinstance(json_data, ALLOWED_TYPES) and json_data is not None:
+        raise TypeError(f'Expected one of {ALLOWED_TYPES + None}. Instead, it is {type(json_data)}')
 
     if isinstance(indent, int):
         indent = ' ' * indent
 
-    lines = []
-
-    for k, v in data.items():
-        if isinstance(v, BASE_TYPES):
-            lines.append(f'{prefix}{k}: {v}')
-
-        if v is None:
-            lines.append(f'{prefix}{k}: {None}')
-
-        if isinstance(v, (Dict, List, Tuple)):
-            lines.append(f'{prefix}{k}:')
-            lines.extend(_prettify(v, 1, indent_str=indent, prefix=prefix))
-
-    return new_line.join(lines)
-
-
-def _prettify(data: Any, depth: int, indent_str: str = '\t', prefix: str = ''):
-    lines = []
-
-    full_prefix = f'{indent_str * depth}{prefix}'
-
-    if isinstance(data, BASE_TYPES):
-        lines.append(f'{full_prefix}{data}')
-
-    elif data is None:
-        lines.append(f'{full_prefix}{None}')
-
-    elif isinstance(data, (List, Tuple)):
-        if data:
-            if len(data) == 1:
-                lines.append(f'{full_prefix}{data[0]}')
-            else:
-                for entry in data:
-                    lines.extend(_prettify(entry, depth, indent_str=indent_str, prefix=prefix))
-
-    elif isinstance(data, Dict):
-        if data:
-            for k, v in data.items():
-                if isinstance(v, BASE_TYPES):
-                    lines.append(f'{full_prefix}{k}: {v}')
-
-                elif v is None:
-                    lines.append(f'{full_prefix}{k}: {None}')
-
-                else:
-                    lines.append(f'{full_prefix}{k}:')
-                    lines.extend(_prettify(v, depth + 1, indent_str=indent_str, prefix=prefix))
-
-    return lines
+    return new_line.join(_serialize(json_data, 0))
--- tests/test_pretty_json.py
+++ tests/test_pretty_json.py
@@ -4,19 +4,46 @@ from human_json import to_human_json
 
 
 @pytest.mark.parametrize('args', [
-    'string',
-    '"json_string"',
-    1,
-    1.1,
-    True,
-    False,
-    None,
-    ['a'],
-    ('a',)
+    set()
 ])
 def test_non_dict(args):
     with pytest.raises(TypeError):
         to_human_json(args)
+
+
+@pytest.mark.parametrize('args,expected', [
+    (1, '1'),
+    (-5.1, '-5.1'),
+    (True, 'True'),
+    (None, 'None')
+])
+def test_literals(args, expected):
+    result = to_human_json(args)
+    assert result == expected
+
+
+def test_dict():
+    result = to_human_json({
+        'key1': 'value1',
+        'key2': (1, 2),
+        'key3': None
+    })
+
+    expected = 'key1: value1\nkey2:\n\t1\n\t2\nkey3: None'
+
+    assert result == expected
+
+
+def test_list():
+    result = to_human_json(['a', 'b', None, True, 1])
+    expected = 'a\nb\nNone\nTrue\n1'
+    assert result == expected
+
+
+def test_tuple():
+    result = to_human_json(('a', 'b', None, True, 1))
+    expected = 'a\nb\nNone\nTrue\n1'
+    assert result == expected
 
 
 @pytest.mark.parametrize('args,expected', [
@@ -30,21 +57,9 @@ def test_non_dict(args):
     ({'key': False}, 'key: False'),
     ({'key': None}, 'key: None')
 ])
-def test_simple_types(args, expected):
+def test_simple_dict(args, expected):
     res = to_human_json(args)
     assert res == expected
-
-
-def test_simple_dict():
-    result = to_human_json({
-        'key1': 'value1',
-        'key2': (1, 2),
-        'key3': None
-    })
-
-    expected = 'key1: value1\nkey2:\n\t1\n\t2\nkey3: None'
-
-    assert result == expected
 
 
 def test_nested_dict():
@@ -64,6 +79,7 @@ def test_nested_dict():
                 '\tin_key2: False\n'
                 '\tin_key3: None\n'
                 '\tin_key4:\n'
+                '\t\t-\n'
                 '\tin_key5:\n'
                 '\t\ta\n'
                 '\t\tb\n'
@@ -106,6 +122,7 @@ def test_nested_with_prefix():
                 '\t\t* average_grade: None\n'
                 '\t\t* description: Complete Assignment 2\n'
                 '\t\t* grades:\n'
+                '\t\t\t* -\n'
                 '* students:\n'
                 '\t* student1\n'
                 '\t* student2\n'
